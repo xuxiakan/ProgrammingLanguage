bcd859f91be9e16eeed4824da4dd477ec6b3bcd4
xuxiakan <xuxiakan@gmail.com>
2017-02-11 19:17:39 -0500

    Init

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,16 @@
+package com.example;
+
+
+/**
+ * Created by xuxiak on 2017/2/11.
+ */
+
+public class Main {
+    public static void main(String args[]) {
+
+
+            Tokenizer tokens = new Tokenizer("program1.core");
+            System.out.println(tokens.currentToken);
+
+    }
+}
